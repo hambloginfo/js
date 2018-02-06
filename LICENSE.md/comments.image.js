c4bd02527b14c5f047ec2e75d1af0a2d1b6db047
Hamlet Xojoyan <32978372+hambloginfo@users.noreply.github.com>
2018-02-06 21:50:07 +0300

    Create comments.image.js

---
+++ LICENSE.md/comments.image.js
@@ -0,0 +1,26 @@
+<body>
+<style>*{margin:0}#b_script_186215{margin-top:0px}</style>
+<div id="b_script_186215">
+
+
+
+<script type="text/javascript" src="//static.criteo.net/js/ld/publishertag.js"></script>
+<script type="text/javascript">
+Criteo.DisplayAd({
+    "zoneid": 532762,
+    "async": false});
+</script><script type="text/javascript" src="http://cas.criteo.com/delivery/ajs.php?ptv=41&amp;zoneid=532762&amp;cb=83931424793&amp;nodis=1&amp;charset=UTF-8&amp;dc=3&amp;atfr=1&amp;loc=http%3A%2F%2Fdzmeruk.com%2FGG%2FP-Christina-Yeghoyan-Che-Che.php"></script><div id="beacon_e96b482649" style="position: absolute; left: 0px; top: 0px; visibility: hidden;">
+<img width="0" height="0" src="http://cat.nl.eu.criteo.com/delivery/lg.php?cppv=1&amp;cpp=rdj%2BWHxHbTZjMzhjRkZOZ1piWjdkQ1NBUTd6SWhpTjNocVBJRCttNHhyRUlJa2p4WGZkdFIrNTBMdGFiMUlhMzd5RHh4OVBFbmZaUG15dWpOVFZ0a2NxWmhEbUdWd3Z3dWJ2enlHbEQwN0pyaHRBeFp2T0JtYzdVOWRSeGVYQUdlNUNtMG1LcFp4VzBnUW9SVnd1aGpxR2Nqb2tJclVQb3lXQkFwcGIrSlRwU3c2MkpmZHM4SGtxRWg1aXRMbmlYTFpLTzFVcEZ2M1FNZlM3K1dRVGtFQ0FLeXdkalNGdEM4OFcxb1MyaGYxOXBSZmFoY2RtcjRNUXVxdVh5MFdzQ255U2JGfA%3D%3D">
+</div>
+
+
+
+
+<p style="text-align: left; margin-left: 15px; margin-top: 35px;"><img src="http://4.bp.blogspot.com/-W3zNsNYxwOE/Wnn4pMXsyUI/AAAAAAAAB7A/qC8o8Re2SMIx0wjA7FAdViotC02Hj0HhACK4BGAYYCw/s1600/commentsV1.png" alt="" width="649" height="48"></p>
+
+
+
+
+
+
+</div></body>
